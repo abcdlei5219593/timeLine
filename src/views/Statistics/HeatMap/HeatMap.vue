b049e024d1786a481f1e7df96025db6e43e48575
yinyan <415453583@qq.com>
2023-06-11 00:14:22 +0800

    fix: 热力图不显示修改

--- src/views/Statistics/HeatMap/HeatMap.vue
+++ src/views/Statistics/HeatMap/HeatMap.vue
@@ -16,20 +16,20 @@ const url = window.location.href;
 if (url.indexOf('/app/wind') >= 1) {
     max.value = 10;
 } else if (url.indexOf('/app/rain') >= 1) {
-    max.value = 5;
+    max.value = 10;
 } else if (url.indexOf('/app/city') >= 1) {
-    max.value = 5;
+    max.value = 10;
 }
 
 const props = defineProps<{
     searchForm: object
 }>();
 const store = useSettingStore();
-const timeLine = reactive([]);
+const timeLine = ref([]);
 const deviceList = ref([]);
 const dataSet = ref({
     data: [],
-    max: 100
+    max: max.value
 });
 
 const gradient = {
@@ -44,25 +44,36 @@ const gradient = {
     0.9: '#BE2102',
     1.0: '#7A0403'
 };
-let timer;
+const timer: any = ref(null);
 watch(
     () => props.searchForm,
     (val) => {
+        if (url.indexOf('/app/city') >= 1) {
+            if (props.searchForm.measure === 'dust') {
+                max.value = 100;
+            } else if (props.searchForm.measure === 'db') {
+                max.value = 150;
+            } else if (props.searchForm.measure === 'effluent') {
+                max.value = 10;
+            }
+        }
         getDeviceDataHandler();
+
     },
     {
         deep: true
     }
 );
+
 const start = () => {
-    if (timeLine.length) {
-        if (timer) {
-            clearInterval(timer);
+    if (timeLine.value.length) {
+        if (timer.value) {
+            clearInterval(timer.value);
         }
         let idx = 0;
-        timer = setInterval(() => {
-            dataSet.value = timeLine[idx];
-            if (idx === timeLine.length - 1) {
+        timer.value = setInterval(() => {
+            dataSet.value = timeLine.value[idx];
+            if (idx === timeLine.value.length - 1) {
                 idx = 0;
             } else {
                 idx++;
@@ -72,6 +83,8 @@ const start = () => {
 };
 
 const getDeviceDataHandler = async () => {
+    clearInterval(timer.value);
+    timeLine.value = [];
     const params = {
         deviceId: deviceList.value.map(({ deviceId }) => deviceId),
         startTime: props.searchForm.date.length ? `${props.searchForm.date[0]} 00:00:00` : '',
@@ -88,7 +101,7 @@ const getDeviceDataHandler = async () => {
     }
     for (const [index, timeData] of data[0].data.entries()) {
         const temp = {
-            max: max,
+            max: max.value,
 
             time: timeData.time,
             data: data.map(item => ({
@@ -97,8 +110,9 @@ const getDeviceDataHandler = async () => {
                 count: item.data[index].avg
             }))
         };
-        console.log(temp, 'temptemp');
-        timeLine.push(temp);
+
+        timeLine.value.push(temp);
+        console.log(timeLine, 'temptemp');
     }
     start();
 
