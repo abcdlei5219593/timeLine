0f6605b4f7dae444846012e8a6416bc39daaf22a
leiming <295166213@qq.com>
2025-06-10 09:48:22 +0800

    build: 删除未使用的 404 页面代码
    - 移除了 dist/assets 目录下的 404.c22e286d.js 和对应的 map 文件
    - 优化了 index.03be6477.js 文件，删除了未使用的引用和代码

---
+++ dist/assets/404.8b16a82d.js
@@ -0,0 +1,2 @@
+import{k as e,p as a}from"./index.2e85093c.js";var t=()=>e("div",null,[a("404")]);export{t as default};
+//# sourceMappingURL=404.8b16a82d.js.map
--- dist/assets/404.c22e286d.js
+++
@@ -1,2 +0,0 @@
-import{k as e,p as a}from"./index.c9d020dc.js";var t=()=>e("div",null,[a("404")]);export{t as default};
-//# sourceMappingURL=404.c22e286d.js.map
